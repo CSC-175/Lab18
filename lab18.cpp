fba21b9b9283746596bd0e630d179f0c6610b734
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2024-11-17 12:38:57 -0500

    Initial Commit

--- lab18.cpp
+++ lab18.cpp
@@ -1,26 +1,5 @@
-#include <iostream>
-using namespace std;
-
 // Definition of printArray function goes here
-template <typename X, typename Z>
-void printArray(X *array, Z size) {
-    for (int i = 0; i < size; i++) {
-        cout << array[i] << " ";
-    }
-}
+
 
 // Definition of sortDn function goes here
-template <typename A, typename B>
-void sortDn(A *array, B size){
-    A temp;
-    for(int alpha=0;alpha<size;alpha++){
-        for(int beta=alpha+1;beta<size;beta++){
-            if (array[beta] > array[alpha]){
-                temp = array[alpha];
-                array[alpha] = array[beta];
-                array[beta] = temp;
-            }
-        }
-    }
-}
 
